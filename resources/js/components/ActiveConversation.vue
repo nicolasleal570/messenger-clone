eaa3ab8695097409033f8327b3612710c6a1bcf5
Nicolas Leal <lealnicolas96@gmail.com>
2019-08-19 14:09:23 -0400

    Arreglando bug del scroll en los chats

--- resources/js/components/ActiveConversation.vue
+++ resources/js/components/ActiveConversation.vue
@@ -1,71 +1,85 @@
 <template>
-    <b-row class="h-100">
-        <b-col cols="8" class="h-100">
-            <b-card title="Conversación Activa" class="h-100">
+  <b-row class="h-100">
+    <b-col cols="8" class="h-100">
+      <b-card class="h-100" no-body>
+        <b-card-body title="Conversación Activa" class="card-body-scroll">
+          <message-conversation-component
+            v-for="message in messages"
+            :key="message.id"
+            :written-by-me="message.written_by_me"
+          >{{ message.content }}</message-conversation-component>
+        </b-card-body>
 
-                <message-conversation-component 
-                    v-for="message in messages"
-                    :key="message.id"
-                    :written-by-me="message.written_by_me"
-                >
-                    {{ message.content }}
-                </message-conversation-component>
+        <!-- BTN ENVIAR MENSAJE -->
+        <div slot="footer">
+          <b-form @submit.prevent="postMessage" class="mb-0">
+            <b-input-group>
+              <b-form-input
+                autocomplete="off"
+                type="text"
+                v-model="newMessage"
+                placeholder="Escribe tu mensaje..."
+              ></b-form-input>
+              <b-input-group-append>
+                <b-btn type="submit" variant="primary">Enviar</b-btn>
+              </b-input-group-append>
+            </b-input-group>
+          </b-form>
+        </div>
+      </b-card>
+    </b-col>
 
-                <!-- BTN ENVIAR MENSAJE -->
-                <div slot="footer">
-                    <b-form @submit.prevent="postMessage" class="mb-0">
-                        <b-input-group>
-                            <b-form-input autocomplete="off" type="text" v-model="newMessage" placeholder="Escribe tu mensaje..."></b-form-input>
-                            <b-input-group-append>
-                                <b-btn type="submit" variant="primary">Enviar</b-btn>
-                            </b-input-group-append>
-                        </b-input-group>
-                    </b-form>
-                </div>
-            </b-card>
-        </b-col>
-
-        <b-col cols="4">
-            <b-img rounded="circle"
-            blank width="60" height="60"
-            blank-color="#777" alt="Person"    
-            ></b-img>
-            <p>{{ contactName }}</p>
-            <hr>
-            <b-form-checkbox>Desactivar notificaciones</b-form-checkbox>
-        </b-col>
-    </b-row>
+    <b-col cols="4">
+      <b-img rounded="circle" blank width="60" height="60" blank-color="#777" alt="Person"></b-img>
+      <p>{{ contactName }}</p>
+      <hr />
+      <b-form-checkbox>Desactivar notificaciones</b-form-checkbox>
+    </b-col>
+  </b-row>
 </template>
+<style>
+.card-body-scroll {
+  max-height: calc(100vh - 63px);
+  overflow-y: auto;
+}
+</style>
+
 <script>
-    export default {
-        props: {
-            contactId : Number,
-            contactName: String,
-            messages: Array,
-        },
-        data(){
-            return{
-                newMessage: '',
-            }
-        },
-        mounted(){
-            // this.getMessages();
-        },
-        methods: {
-            postMessage(){
-                const params = {
-                    to_id: this.contactId,
-                    content: this.newMessage,
-                }
-                axios.post('/api/messages', params)
-                    .then((response) => {
-                        // console.log(response.data);
-                        if (response.data) {
-                            this.newMessage = '';
-                            // this.getMessages();
-                        }
-                    });
-            }
+export default {
+  props: {
+    contactId: Number,
+    contactName: String,
+    messages: Array
+  },
+  data() {
+    return {
+      newMessage: ""
+    };
+  },
+  mounted() {
+    // this.getMessages();
+  },
+  methods: {
+    postMessage() {
+      const params = {
+        to_id: this.contactId,
+        content: this.newMessage
+      };
+      axios.post("/api/messages", params).then(response => {
+        // console.log(response.data);
+        if (response.data) {
+          this.newMessage = "";
+          // this.getMessages();
         }
+      });
+    },
+    scrollToBottom() {
+      const el = document.querySelector(".card-body-scroll");
+      el.scrollTop = el.scrollHeight;
     }
+  },
+  updated() {
+    this.scrollToBottom();
+  }
+};
 </script>
